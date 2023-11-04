bfc7264dcb09ce8c1d922283c3662dd3ee0d61a2
belloLucas <lucasbellodev@gmail.com>
2023-11-04 14:41:02 -0300

    feat: Enroll component finished, minor fixes also on Courses padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header.jsx";
 import HeaderBody from "./components/header/HeaderBody.jsx";
 import Courses from "./components/main/Courses.jsx";
+import Enroll from "./components/enroll/Enroll";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header></Header>
       <HeaderBody></HeaderBody>
       <Courses></Courses>
+      <Enroll></Enroll>
     </>
   );
 }
---
+++ src/components/enroll/Enroll.jsx
@@ -0,0 +1,27 @@
+import "./styles/enroll.css";
+
+import woman from "../../assets/enroll/woman.png";
+
+export default function Enroll() {
+  return (
+    <section className="enroll">
+      <img src={woman} alt="" />
+      <div className="texts">
+        <h1>
+          Construa seu futuro em uma das <br /> melhores escolas de tecnologia
+          do <br /> Brasil
+        </h1>
+        <p className="text">
+          Nossa escola de cursos de tecnologia oferece programas de formação de
+          alta qualidade para ajudar os alunos a alcançarem seus objetivos e se
+          destacarem no mercado. Com uma equipe de instrutores altamente
+          qualificados e tecnologias avançadas, nós preparamos nossos alunos
+          para o futuro da tecnologia.
+        </p>
+        <a className="btn" href="#">
+          Matricular agora!
+        </a>
+      </div>
+    </section>
+  );
+}
